426daf586791a439f5e3e4d9d8d29a0cda1e6555
sean <sean@gomicorp.com>
2022-08-24 20:50:41 +0900

    berry setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import './index.css';
-import 'lazysizes';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  ...require('config/tailwind.config'),
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', '../../packages/ui/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui'), require('@tailwindcss/line-clamp')],
 };
